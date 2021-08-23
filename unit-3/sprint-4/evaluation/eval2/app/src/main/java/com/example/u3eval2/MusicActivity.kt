2969b1210cf69abc7f2345ca8a07130e44e8fcd0
dudi111 <pd.rajiv.000@gmail.com>
2021-08-23 17:37:00 +0530

    commit9

--- unit-3/sprint-4/evaluation/eval2/app/src/main/java/com/example/u3eval2/MusicActivity.kt
+++ unit-3/sprint-4/evaluation/eval2/app/src/main/java/com/example/u3eval2/MusicActivity.kt
@@ -1,20 +1,12 @@
 package com.example.u3eval2
 
+import android.content.Intent
 import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_music.*
 
 class MusicActivity : AppCompatActivity() {
-
-
-   fun serviceconnection:ServiceConnection{
-
-        onser
-
-
-
-    }
 
 
 
@@ -26,5 +18,13 @@ class MusicActivity : AppCompatActivity() {
         tvtrack1.setText(intent.getString("name"))
         tvartist1.setText(intent.getString("artist"))
         tvcollection1.setText(intent.getString("collection"))
+
+        val intent1= Intent(this,MusicService::class.java)
+        btnPlay.setOnClickListener {
+            startService(intent1)
+        }
+        btnPause.setOnClickListener {
+            stopService(intent1)
+        }
     }
 }
--- unit-3/sprint-4/evaluation/eval2/app/src/main/java/com/example/u3eval2/MusicService.kt
+++ unit-3/sprint-4/evaluation/eval2/app/src/main/java/com/example/u3eval2/MusicService.kt
@@ -1,47 +1,76 @@
 package com.example.u3eval2
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
-import android.os.Binder
+import android.os.Build
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import java.util.*
 
 class MusicService : Service() {
 
     private lateinit var mediaPlayer:MediaPlayer
 
-    fun MusicService(){
 
-    }
+
+
 
     override fun onCreate() {
         super.onCreate()
         mediaPlayer= MediaPlayer.create(this,R.raw.dawood)
-    }
-    fun play(){
-        if (!mediaPlayer.isPlaying){
-            mediaPlayer.start()
-        }
-    }
-    fun pause(){
-        if(mediaPlayer.isPlaying){
-            mediaPlayer.pause()
-        }
+        showNotificationAndStartForeGround()
     }
 
-    override fun onBind(intent: Intent): IBinder {
-        return Servicebinder()
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        mediaPlayer.start()
+        return super.onStartCommand(intent, flags, startId)
     }
+
+
+
 
     override fun onDestroy() {
         super.onDestroy()
-        mediaPlayer.release()
+        mediaPlayer.stop()
     }
-    class Servicebinder:Binder(){
-        fun MusicService(){
-            return MusicService()
+
+    override fun onBind(intent: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+
+    private fun showNotificationAndStartForeGround() {
+        createChannel()
+        var notificationBuilder: NotificationCompat.Builder? = null
+        notificationBuilder = NotificationCompat.Builder(this, "CHANNEL_ID")
+            .setContentTitle("Music is Playing")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+        var notification: Notification? = null
+        notification = notificationBuilder.build()
+        startForeground(1, notification)
+    }
 
 
+    /*
+Create noticiation channel if OS version is greater than or eqaul to Oreo
+*/
+    fun createChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "CHANNEL_ID",
+                "CHANNEL_NAME",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = "Call Notifications"
+            Objects.requireNonNull(
+                this.getSystemService(
+                    NotificationManager::class.java
+                )
+            ).createNotificationChannel(channel)
         }
     }
+
 }
