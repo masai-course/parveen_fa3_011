3d3713f565d8d563ce7634d7cf3a58fe7f234040
dudi111 <pd.rajiv.000@gmail.com>
2021-09-27 15:34:48 +0530

    Commit4

--- unit-4/sprint-4/evaluation/unit4eval2/app/src/main/java/com/example/unit4eval2/views/MainActivity.kt
+++ unit-4/sprint-4/evaluation/unit4eval2/app/src/main/java/com/example/unit4eval2/views/MainActivity.kt
@@ -2,14 +2,44 @@ package com.example.unit4eval2.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.unit4eval2.R
+import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity(), onAddclicked {
+
+    private lateinit var pizamodel:List<PizzaModel>
+    private lateinit var pizasize:List<Size>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+
+
+    }
+    fun ApiCall(){
+        val apiclient=Network.getinstance().create(ApiService::class.java)
+        apiclient.posts("pizzas").enqueue(object :Callback<PizzaModel>{
+            override fun onResponse(call: Call<PizzaModel>, response: Response<PizzaModel>) {
+              pizamodel=response?.body()
+            }
+
+            override fun onFailure(call: Call<PizzaModel>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
+    private fun setrecycler(){
+        val pizaadapter=PizzaAdapter(pizamodel,pizasize,this)
+        val layoutManager=LinearLayoutManager(this)
+        Recycler.adapter=pizaadapter
+        Recycler.layoutManager=layoutManager
     }
 
     override fun onitemClicked(position: Int, pizamodel: PizzaModel) {
