5689c4f3d03bd3fd81584c70506d4015cf6c90b5
dudi111 <pd.rajiv.000@gmail.com>
2021-11-08 14:53:41 +0530

    news2

---
+++ unit-4/evaluation/U5eval2/app/src/main/java/com/example/u5eval2/ApiService.kt
@@ -0,0 +1,10 @@
+package com.example.u5eval2
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("v2/everything?q=tesla&from=2021-10-08&sortBy=publishedAt&apiKey=480166648b5a441eb890253626e776cb")
+     fun getposts(@Query("tesla")tesla:String) :List<ResponseModel>
+}
---
+++ unit-4/evaluation/U5eval2/app/src/main/java/com/example/u5eval2/NewsAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.u5eval2
+
+class NewsAdapter {
+}
