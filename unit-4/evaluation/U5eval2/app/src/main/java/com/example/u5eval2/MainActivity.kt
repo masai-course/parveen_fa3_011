014ae5c2e3e126d7109944d079e6c9952516923d
dudi111 <pd.rajiv.000@gmail.com>
2021-11-08 16:39:25 +0530

    news5

--- unit-4/evaluation/U5eval2/app/src/main/java/com/example/u5eval2/MainActivity.kt
+++ unit-4/evaluation/U5eval2/app/src/main/java/com/example/u5eval2/MainActivity.kt
@@ -7,12 +7,18 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.u5eval2.data.Article
 import com.example.u5eval2.data.ResponseModel
 import com.example.u5eval2.viewModel.ServiceNetwork
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers.io
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var newsAdapter: NewsAdapter
     private lateinit var recyclerView: RecyclerView
     private val resp=ArrayList<Article>()
+    private lateinit var disposable: Disposable
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,9 +26,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         recyclerView=findViewById(R.id.recycler)
 
+
     }
     private fun apicall(){
-        ServiceNetwork.getapi().getposts().subs
+        ServiceNetwork.getapi().getposts().subscribeOn(Scheduler.io).observeOn(AndroidSchedulers.mainThread())
+            .subscribe(object :Observer<Article>{
+                override fun onSubscribe(d: Disposable) {
+                    d.let {
+                      disposable=it
+                            }                }
+
+                override fun onNext(t: Article) {
+                 setadaptr()
+               }
+
+                override fun onError(e: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onComplete() {
+                    TODO("Not yet implemented")
+                }
+
+            })
     }
 
     private fun setadaptr(){
