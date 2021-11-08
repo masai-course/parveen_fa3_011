faf8ca92409d932198a1679fce0938be7b5052c2
dudi111 <pd.rajiv.000@gmail.com>
2021-11-08 15:32:57 +0530

    news3

--- unit-4/evaluation/U5eval2/app/src/main/java/com/example/u5eval2/NewsAdapter.kt
+++ unit-4/evaluation/U5eval2/app/src/main/java/com/example/u5eval2/NewsAdapter.kt
@@ -1,4 +1,31 @@
 package com.example.u5eval2
 
-class NewsAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
+
+class NewsAdapter(val newslist:List<Article>):RecyclerView.Adapter<newsViewHOlder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): newsViewHOlder {
+val view=LayoutInflater.from(parent.context).inflate(R.layout.newslayout,parent,false)
+    return newsViewHOlder(view)
+    }
+
+    override fun onBindViewHolder(holder: newsViewHOlder, position: Int) {
+      val news=newslist[position]
+news.let {
+    holder.setdata(it)
+}    }
+
+    override fun getItemCount(): Int {
+return newslist.size   }
+
+
 }
--- unit-4/evaluation/U5eval2/app/src/main/java/com/example/u5eval2/NewsViewHolder.kt
+++
@@ -1,4 +0,0 @@
-package com.example.u5eval2
-
-class NewsViewHolder {
-}
