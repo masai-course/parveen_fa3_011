3e6971ac6960834165480424cb688930257fd7ab
dudi111 <pd.rajiv.000@gmail.com>
2022-02-04 13:58:59 +0530

    gamefinal

--- unit-4/sprint-4/day-2/PickGrey/app/src/main/java/com/example/pickgrey/MainActivity.kt
+++ unit-4/sprint-4/day-2/PickGrey/app/src/main/java/com/example/pickgrey/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.pickgrey
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.example.pickgrey.databinding.ActivityMainBinding
@@ -19,6 +20,8 @@ class MainActivity : AppCompatActivity() {
     private var list= listOf<Int>(0,1,2,3)
     private var num=0
      private var boo:Boolean =true
+     lateinit var  myhandler: Handler
+     lateinit var myrunable: Runnable
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +29,16 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding= DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         startgame()
+        myhandler = Handler()
+        myrunable= Runnable {
+            kotlin.run {
+                boo=false
+                activityMainBinding.cvcard.visibility = View.VISIBLE
+                activityMainBinding.tvtotalscore.text = "TOTAL SCORE: $count"
+                count=0
+            }
+        }
+        starthandler()
 
         activityMainBinding.view0.setOnClickListener {
             if (num==0){
@@ -90,21 +103,24 @@ class MainActivity : AppCompatActivity() {
         if (random==0){
             activityMainBinding.view0.setBackgroundColor(Color.parseColor("#808080"))
             delay(1000)
-            activityMainBinding.view0.setBackgroundColor(Color.BLACK)
+            activityMainBinding.view0.setBackgroundColor(Color.parseColor("#fb2112"))
         }else if (random==1){
             activityMainBinding.view1.setBackgroundColor(Color.parseColor("#808080"))
             delay(1000)
-            activityMainBinding.view1.setBackgroundColor(Color.BLUE)
+            activityMainBinding.view1.setBackgroundColor(Color.parseColor("#3235ac"))
         }else if (random==2){
             activityMainBinding.view2.setBackgroundColor(Color.parseColor("#808080"))
             delay(1000)
-            activityMainBinding.view2.setBackgroundColor(Color.parseColor("#f83b18"))
+            activityMainBinding.view2.setBackgroundColor(Color.parseColor("#f6f93b"))
         }else if (random==3){
             activityMainBinding.view3.setBackgroundColor(Color.parseColor("#808080"))
             delay(1000)
-            activityMainBinding.view3.setBackgroundColor(Color.parseColor("#f6f93b"))
+            activityMainBinding.view3.setBackgroundColor(Color.parseColor("#02991b"))
         }
+
+
     }
+
 
     fun startgame(){
         CoroutineScope(Dispatchers.Main).launch {
@@ -112,5 +128,17 @@ class MainActivity : AppCompatActivity() {
                 getrandomnum()
             }
         }
+    }
+
+    override fun onUserInteraction() {
+        super.onUserInteraction()
+        stophandler()
+        starthandler()
+    }
+    fun starthandler(){
+        myhandler.postDelayed(myrunable, 1000)
+    }
+    fun stophandler(){
+        myhandler.removeCallbacks(myrunable)
     }
 }
